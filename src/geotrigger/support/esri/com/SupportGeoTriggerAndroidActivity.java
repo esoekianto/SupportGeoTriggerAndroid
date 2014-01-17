05801ee77aef79d130ee61550d14e8829b7bcea8
zhangfengesri <f.feng.zhang@gmail.com>
2014-01-17 10:41:54 -0800

    Initial checking in - this is a Esri Support training project for Geotrigger services.

---
+++ src/geotrigger/support/esri/com/SupportGeoTriggerAndroidActivity.java
@@ -0,0 +1,39 @@
+package geotrigger.support.esri.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.esri.android.map.MapView;
+
+
+public class SupportGeoTriggerAndroidActivity extends Activity {
+	
+	MapView mMapView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		mMapView = new MapView(this);
+
+    }
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+ }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mMapView.pause();
+ }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mMapView.unpause();
+	}
+
+}
